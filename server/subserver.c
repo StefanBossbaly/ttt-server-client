664365c43e1f54c724037213af030e34a7055290
Stefan Bossbaly <sbossb@gmail.com>
2013-10-26 14:24:09 -0700

    Added the subserver routines
    Once we have enough connections to clients the super server will create
    a subserver and pass the socket id's to the subserver. It will now be
    the job of the subserver to handle these connections while the
    superserver will still be listening for new connections.

---
+++ server/subserver.c
@@ -0,0 +1,69 @@
+#include "subserver.h"
+
+#include <unistd.h>
+
+#include <errno.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+void subserver_init(subserver_t *subserver, int *clients, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		//Add the client to our list of clients
+		subserver->clients[i] = clients[i];
+
+		//Add it our file descriptor set
+		FD_SET(subserver->clients[i] , &subserver->master);
+	}
+
+	subserver->clients_size = size;
+}
+
+void subserver_handle(subserver_t *subserver)
+{
+	fd_set temp;
+
+	//Copy our master to our temp
+	memcpy(&temp, &subserver->master, sizeof(subserver->master));
+
+	//Let's see has unhandled commands
+	if (select(subserver->clients_size + 1, &temp, NULL, NULL, NULL) == -1)
+	{
+		printf("Error: Subserver select failed!: %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	int i;
+	for (i = 0; i <= subserver->clients_size; i++)
+	{
+		//We found a socket that needs some processing time
+		if (FD_ISSET(subserver->clients[i], &temp))
+		{
+			//Buffer for receiving a command
+			char buffer[1024];
+
+			//TODO handle a close connection
+			int nbytes = recv(subserver->clients[i], buffer, sizeof(buffer), 0);
+
+			//Call the handler
+			//subserver->handle_recieve(subserver->clients[i], buffer, sizeof(buffer));
+
+			printf("This was received: %s\n", buffer);
+		}
+	}
+}
+
+void subserver_brodcast(subserver_t *subserver, const char *command, size_t size)
+{
+
+}
---
+++ server/subserver.h
@@ -0,0 +1,32 @@
+/*
+ * subserver.h
+ *
+ *  Created on: Oct 26, 2013
+ *      Author: stefan
+ */
+
+#ifndef SUBSERVER_H_
+#define SUBSERVER_H_
+
+#define MAX_CLIENTS 10
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+typedef struct
+{
+	//List of the fd of sockets for our clients
+	int clients[MAX_CLIENTS];
+
+	//How many we have
+	int clients_size;
+
+	fd_set master;
+	int (*handle_recieve)(int id, char *buf, size_t size);
+} subserver_t;
+
+void subserver_init(subserver_t *subserver, int *clients, int size);
+void subserver_handle(subserver_t *subserver);
+void subserver_brodcast(subserver_t *subserver, const char *command, size_t size);
+#endif /* SUBSERVER_H_ */
