66b40672bc43ea7de42a680380d486e19913d69d
Stefan Bossbaly <sbossb@gmail.com>
2013-10-27 10:51:05 -0700

    Added threads for the subserver
    Each subserver should have their own thread for game logic excution.
    Threads make more sense than the system call fork() since they are
    easy to use and take alot less memory then fork(). Need to do better
    thread management but that can be done in a later commit.

--- server/driver.c
+++ server/driver.c
@@ -1,24 +1,69 @@
 #include "server.h"
+#include "subserver.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 #define HOST "localhost\0"
 #define PORT 8080
 #define BACKLOG 10
+#define NUM_THREADS	5
+
+void *thread_server_do_work(void *uncast_subserver)
+{
+	printf("Thread created!\n");
+
+	subserver_t *subserver = (subserver_t *) uncast_subserver;
+
+	while (1)
+	{
+		subserver_handle(subserver);
+	}
+
+	return NULL;
+}
 
 int main()
 {
+	//Create server thread and subserver threads
+	pthread_t subserver_threads[NUM_THREADS];
+	int i = 0;
+	pthread_attr_t attr;
+
+	/* Initialize and set thread detached attribute */
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+	//Create a server and start listening
 	server_t server;
-
 	server_init(&server, "127.0.0.1", 8080, 10);
-
 	server_start(&server);
 
-	printf("Socket ID: %i\n", server.socket_id);
-
-	while(1)
+	while (1)
 	{
 		server_handle(&server);
+
+		if (server.client_size == 2)
+		{
+			printf("Creating thread!\n");
+
+			subserver_t *subserver = (subserver_t *) malloc(sizeof(subserver_t));
+
+			subserver_init(subserver, server.clients, 2);
+
+			int resultCode = pthread_create(&subserver_threads[i], &attr, thread_server_do_work, subserver);
+
+			if (resultCode)
+			{
+				printf("ERROR; return code from pthread_create() is %d\n", resultCode);
+				exit(-1);
+			}
+
+			//Create a new thread
+			server.client_size = 0;
+			i++;
+		}
 	}
 
 	return 0;
