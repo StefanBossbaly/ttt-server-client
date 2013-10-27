8358eafd9bb5b08765920a3374de57d0d5809865
Stefan Bossbaly <sbossb@gmail.com>
2013-10-27 10:56:06 -0700

    Have the subserver handle the clients
    The subserver should handle the clients for each game. They will
    have their own thread of execution.

--- server/subserver.c
+++ server/subserver.c
@@ -15,18 +15,35 @@
 
 void subserver_init(subserver_t *subserver, int *clients, int size)
 {
-	int i;
+	FD_ZERO(&subserver->master);
 
+	int i;
 	for (i = 0; i < size; i++)
 	{
 		//Add the client to our list of clients
 		subserver->clients[i] = clients[i];
 
 		//Add it our file descriptor set
-		FD_SET(subserver->clients[i] , &subserver->master);
+		FD_SET(subserver->clients[i], &subserver->master);
 	}
 
 	subserver->clients_size = size;
+}
+
+int subserver_max_fd(subserver_t *subserver)
+{
+	int max = -1;
+
+	int i;
+	for (i = 0; i < subserver->clients_size; i++)
+	{
+		if (subserver->clients[i] > max)
+		{
+			max = subserver->clients[i];
+		}
+	}
+
+	return max;
 }
 
 void subserver_handle(subserver_t *subserver)
@@ -37,14 +54,14 @@ void subserver_handle(subserver_t *subserver)
 	memcpy(&temp, &subserver->master, sizeof(subserver->master));
 
 	//Let's see has unhandled commands
-	if (select(subserver->clients_size + 1, &temp, NULL, NULL, NULL) == -1)
+	if (select(subserver_max_fd(subserver) + 1, &temp, NULL, NULL, NULL) == -1)
 	{
 		printf("Error: Subserver select failed!: %s", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 
 	int i;
-	for (i = 0; i <= subserver->clients_size; i++)
+	for (i = 0; i < subserver->clients_size; i++)
 	{
 		//We found a socket that needs some processing time
 		if (FD_ISSET(subserver->clients[i], &temp))
@@ -58,7 +75,21 @@ void subserver_handle(subserver_t *subserver)
 			//Call the handler
 			//subserver->handle_recieve(subserver->clients[i], buffer, sizeof(buffer));
 
-			printf("This was received: %s\n", buffer);
+			int j;
+			for (j = 0; j < subserver->clients_size; j++)
+			{
+				// send to everyone!
+				if (FD_ISSET(subserver->clients[j], &subserver->master))
+				{
+					// except the listener and ourselves
+					if (send(subserver->clients[j], buffer, nbytes, 0) == -1)
+					{
+						perror("send");
+					}
+				}
+
+				printf("This was received: %s\n", buffer);
+			}
 		}
 	}
 }
--- server/subserver.h
+++ server/subserver.h
@@ -29,4 +29,5 @@ typedef struct
 void subserver_init(subserver_t *subserver, int *clients, int size);
 void subserver_handle(subserver_t *subserver);
 void subserver_brodcast(subserver_t *subserver, const char *command, size_t size);
+int subserver_max_fd(subserver_t *subserver);
 #endif /* SUBSERVER_H_ */
