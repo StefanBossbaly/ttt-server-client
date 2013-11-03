738feb9638095a23f1ca8b64ecd8025e173e7344
Zachary Haupt <zachary.haupt@scranton.edu>
2013-11-03 18:48:07 -0500

    added comments

--- client/TicTacToeRunner.java
+++ client/TicTacToeRunner.java
@@ -7,27 +7,22 @@ import javax.swing.UIManager;
  */
 public class TicTacToeRunner {
  
-    /**
-     * @param args the command line arguments
-     */
+    /*
+      @param args the command line arguments
+    */
     public static void main(String[] args) 
-    {
+   {
         try
-	{
-		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	}
-	catch (Exception e)
-	{
-		e.printStackTrace();
-	}
-	JFrame frame = new TicTacToeFrame();
-        //JFrame frame1 = new ChatClient();
-        //frame1.setTitle("Chat Window");
+	   {
+		   UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	   }
+	   catch (Exception e)
+	   {
+		   e.printStackTrace();
+	   }
+	   JFrame frame = new TicTacToeFrame();
         frame.setTitle("Tic Tac Toe Game");
-        //frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame1.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
- 
+        frame.setVisible(true); 
     }
 }
