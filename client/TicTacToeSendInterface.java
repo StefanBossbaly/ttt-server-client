29657033ef02e05166c89ee47f9a9df79a38d41e
Stefan Bossbaly <sbossb@gmail.com>
2013-11-03 15:42:26 -0800

    Added an interface for sending data to the server
    This interface will send data to the server

---
+++ client/TicTacToeSendInterface.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class TicTacToeSendInterface {
+	private Socket socket;
+	private OutputStreamWriter writer;
+
+	public TicTacToeSendInterface(Socket socket) throws IOException {
+		this.socket = socket;
+		this.writer = new OutputStreamWriter(this.socket.getOutputStream(),
+				Charset.forName("US-ASCII"));
+	}
+
+	public void sendMoveCommand(int x, int y) {
+		try {
+			String moveCommand = "MOVE " + x + " " + y + "\0";
+			writer.write(moveCommand);
+			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
