ed280a557a4f6c8b1d74317f61a9f449d1e676bf
Stefan Bossbaly <sbossb@gmail.com>
2013-11-03 21:08:12 -0500

    Added documentation

--- client/tttclient/serverinterface/TicTacToeRecieveInterface.java
+++ client/tttclient/serverinterface/TicTacToeRecieveInterface.java
@@ -11,11 +11,31 @@ import tttclient.handler.EndCommandHandler;
 import tttclient.handler.ErrorCommandHandler;
 import tttclient.handler.MoveCommandHandler;
 
+/**
+ * Class that is the link between the server and the client using the designed
+ * protocol
+ * 
+ * @author Stefan Bossbaly
+ * @author Hardik Patel
+ * 
+ */
 public class TicTacToeRecieveInterface {
+
+	/**
+	 * The regex pattern for the move command
+	 */
 	public static final Pattern MOVE_COMMAND = Pattern
 			.compile("MOVE[\\s]+(?<x>\\d+)[\\s]+(?<y>\\d+)[\\s]+(?<player>\\d+)");
+
+	/**
+	 * The regex pattern for the error command
+	 */
 	public static final Pattern ERROR_COMMAND = Pattern
 			.compile("ERROR[\\s]+(?<error>[\\d]+)");
+
+	/**
+	 * The regex pattern for the end command
+	 */
 	public static final Pattern END_COMMAND = Pattern
 			.compile("END[\\s]+(?<status>[\\d]+)[\\s]+(?<player>[\\d]+)");
 
@@ -25,6 +45,12 @@ public class TicTacToeRecieveInterface {
 	private ErrorCommandHandler errorHandler;
 	private EndCommandHandler endHandler;
 
+	/**
+	 * 
+	 * @param socket
+	 * @throws IOException
+	 *             if the socket's input stream is not valid
+	 */
 	public TicTacToeRecieveInterface(Socket socket) throws IOException {
 		this.socket = socket;
 		this.reader = new InputStreamReader(this.socket.getInputStream(),
@@ -34,18 +60,44 @@ public class TicTacToeRecieveInterface {
 		this.endHandler = null;
 	}
 
+	/**
+	 * Registers a move command handler to receive call backs when a move
+	 * command is received
+	 * 
+	 * @param handler
+	 *            the class that implements the interface
+	 */
 	public void registerMoveCommandHandler(MoveCommandHandler handler) {
 		this.moveHandler = handler;
 	}
 
+	/**
+	 * Registers a error command handler to receive call backs when a error
+	 * command is received
+	 * 
+	 * @param handler
+	 *            the class that implements the interface
+	 */
 	public void registerErrorCommandHandler(ErrorCommandHandler handler) {
 		this.errorHandler = handler;
 	}
 
+	/**
+	 * Registers a end command handler to receive call backs when a end command
+	 * is received
+	 * 
+	 * @param handler
+	 *            the class that implements the interface
+	 */
 	public void registerEndCommandHandler(EndCommandHandler handler) {
 		this.endHandler = handler;
 	}
 
+	/**
+	 * Checks to see if the input stream has something to be read
+	 * 
+	 * @return true if the buffer of the stream is not empty; false otherwise
+	 */
 	public boolean isReady() {
 		try {
 			return reader.ready();
@@ -54,23 +106,35 @@ public class TicTacToeRecieveInterface {
 		}
 	}
 
+	/**
+	 * Tries to process string received as a command
+	 * 
+	 * @throws IOException
+	 */
 	public void acceptInput() throws IOException {
+
+		// Make sure that we are ready for input
 		if (reader.ready()) {
+
+			// protocol defines that messages can't exceed protocol 1024
 			char[] buffer = new char[1024];
 
+			// Read the message into the buffer
 			if (reader.read(buffer, 0, buffer.length) == -1) {
 				return;
 			}
 
+			// Convert char[] into Java String
 			String command = "";
-
 			for (int i = 0; buffer[i] != '\0'; i++) {
 				command += buffer[i];
 			}
 
 			System.out.println("Buffer read: " + command);
+
 			Matcher matcher = null;
 
+			// See if the command matches our move command
 			matcher = MOVE_COMMAND.matcher(command);
 			if (matcher.matches()) {
 				System.out.println("Move Command Recieved");
@@ -84,6 +148,7 @@ public class TicTacToeRecieveInterface {
 				return;
 			}
 
+			// See if the command matches our error command
 			matcher = ERROR_COMMAND.matcher(command);
 			if (matcher.matches()) {
 				System.out.println("Error Command Recieved");
@@ -94,6 +159,7 @@ public class TicTacToeRecieveInterface {
 				return;
 			}
 
+			// See if the command matches our end command
 			matcher = END_COMMAND.matcher(command);
 			if (matcher.matches()) {
 				System.out.println("End Command Recieved");
@@ -104,7 +170,8 @@ public class TicTacToeRecieveInterface {
 				return;
 			}
 
-			System.out.println("Command not reconized");
+			// Command was not recognized
+			throw new RuntimeException("Command not recognized");
 		}
 	}
 }
--- client/tttclient/serverinterface/TicTacToeSendInterface.java
+++ client/tttclient/serverinterface/TicTacToeSendInterface.java
@@ -1,19 +1,42 @@
 package tttclient.serverinterface;
+
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.nio.charset.Charset;
 
+/**
+ * Class that handles the sending of information between the client and the
+ * server via the protocol
+ * 
+ * @author Stefan Bossbaly
+ * @author Hardik Patel
+ * 
+ */
 public class TicTacToeSendInterface {
 	private Socket socket;
 	private OutputStreamWriter writer;
 
+	/**
+	 * Constructs a send interface over the socket
+	 * 
+	 * @param socket
+	 * @throws IOException
+	 */
 	public TicTacToeSendInterface(Socket socket) throws IOException {
 		this.socket = socket;
 		this.writer = new OutputStreamWriter(this.socket.getOutputStream(),
 				Charset.forName("US-ASCII"));
 	}
 
+	/**
+	 * Sends a move command to the server
+	 * 
+	 * @param x
+	 *            the x position of the location
+	 * @param y
+	 *            the y position of the location
+	 */
 	public void sendMoveCommand(int x, int y) {
 		try {
 			String moveCommand = "MOVE " + x + " " + y + "\0";
