07566c0d2a8869f37b8e622df6fcaa3b2b3837d8
Stefan Bossbaly <sbossb@gmail.com>
2013-11-28 15:10:52 -0500

    Moved the size of the name array into a define
    This way we are able to change it at one place and have it carry
    over to all places in the code. Not sure if I will use it like that
    but it is good coding practice and its important to me :D

--- server/player_record.h
+++ server/player_record.h
@@ -8,10 +8,13 @@
 #ifndef PLAYER_RECORD_H_
 #define PLAYER_RECORD_H_
 
+#define LAST_NAME_MAX 10
+#define FIRST_NAME_MAX 10
+
 typedef struct {
    int player_id;
-   char first_name[10];
-   char last_name[10];
+   char first_name[FIRST_NAME_MAX];
+   char last_name[LAST_NAME_MAX];
    int wins;
    int losses;
    int ties;
