74eb44d11b64c97c4b0b60d8cc306f1d6f1d2e7f
Stefan Bossbaly <sbossb@gmail.com>
2013-11-29 09:17:07 -0500

    Fixed semaphores
    This handles the case that both processes try to init the semaphore
    at the same time. Took quite a bit of code but I will have to test
    it out.

--- server/util/semaphore.c
+++ server/util/semaphore.c
@@ -4,56 +4,109 @@
 #include <stdio.h>
 #endif
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-#include <errno.h>
 
-void semaphore_init(semaphore_t *semaphore, int value, int key)
+union semun
 {
-	  //See if there is already a semaphore with the value of key
-	  if ((semaphore->id = semget(key, 1, 0)) == -1)
-	  {
-		  if (errno == ENOENT)
-		  {
-			  semaphore->id = semget(key, 1,  0777 | IPC_CREAT);
-			  union semun arg;
-			  arg.val = value;
-			  semctl(semaphore->id, 0, SETVAL, arg);
+	int val;
+	struct semid_ds *buf;
+	ushort *array;
+};
 
-#ifdef SEMAPHORE_DEBUG
-			  printf("Semaphore created! id = %i, value %i\n", id, value);
-#endif
-		  }
-		  else
-		  {
-			  perror("semaphore_init");
-		  }
-	  }
+int semaphore_init(semaphore_t *semaphore, int value, int key)
+{
+	union semun arg;
+	struct semid_ds buf;
+	struct sembuf sb;
+
+	int semid = semget(key, 1, IPC_CREAT | IPC_EXCL | 0666);
+
+	//We got to the address first
+	if (semid >= 0)
+	{
+		sb.sem_op = 1;
+		sb.sem_flg = 0;
+		arg.val = 1;
+
+		if (semop(semid, &sb, 1) == -1)
+		{
+			perror("semop");
+			semctl(semid, 0, IPC_RMID);
+			return -1;
+		}
+
+		semaphore->id = semid;
+	}
+	else if (errno == EEXIST)
+	{
+		int ready = 0;
+
+		//Get the id of the shared memory
+		semid = semget(key, 1, 0);
+
+		//Error happened check errno
+		if (semid < 0)
+			return -1;
+
+		arg.buf = &buf;
+
+		//Wait for the other process to init the semaphore
+		int i;
+        for (i = 0; i < MAX_RETRIES && !ready; i++)
+        {
+        	//Get statics of the semaphore
+        	semctl(semid, 0, IPC_STAT, arg);
+
+        	//Check to see if the semaphore was created
+            if (arg.buf->sem_otime != 0)
+            {
+                ready = 1;
+            }
+            else
+            {
+                sleep(1);
+            }
+        }
+
+        if (!ready)
+        {
+            errno = ETIME;
+            return -1;
+        }
+
+        semaphore->id = semid;
+	}
+
+	return -1;
 }
 
 void semaphore_remove(semaphore_t *semaphore)
 {
-    union semun arg;
-    semctl(semaphore->id, 0, IPC_RMID, arg);
+	union semun arg;
+	semctl(semaphore->id, 0, IPC_RMID, arg);
 }
 
 void semaphore_wait(semaphore_t *semaphore)
 {
-	  struct sembuf	p_buf;
-	  p_buf.sem_num = 0;
-	  p_buf.sem_op = -1;	     // subtract 1 indicates P
-	  p_buf.sem_flg = 0;
+	struct sembuf p_buf;
+	p_buf.sem_num = 0;
+	p_buf.sem_op = -1;	     // subtract 1 indicates P
+	p_buf.sem_flg = 0;
 
-	  semop(semaphore->id, &p_buf, 1);
+	semop(semaphore->id, &p_buf, 1);
 }
 
 void semaphore_signal(semaphore_t *semaphore)
 {
-	  struct sembuf	v_buf;
-	  v_buf.sem_num = 0;
-	  v_buf.sem_op = 1;          // add 1 indicates V
-	  v_buf.sem_flg = 0;
+	struct sembuf v_buf;
+	v_buf.sem_num = 0;
+	v_buf.sem_op = 1;          // add 1 indicates V
+	v_buf.sem_flg = 0;
 
-	  semop(semaphore->id, &v_buf, 1);
+	semop(semaphore->id, &v_buf, 1);
 }
--- server/util/semaphore.h
+++ server/util/semaphore.h
@@ -8,12 +8,14 @@
 #ifndef SEMAPHORE_H_
 #define SEMAPHORE_H_
 
+#define MAX_RETRIES 10
+
 typedef struct
 {
 	int id;
 } semaphore_t;
 
-void semaphore_init(semaphore_t *semaphore, int value, int key);
+int semaphore_init(semaphore_t *semaphore, int value, int key);
 void semaphore_remove(semaphore_t *semaphore);
 void semaphore_wait(semaphore_t *semaphore);
 void semaphore_signal(semaphore_t *semaphore);
